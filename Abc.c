e882fab861005dfa2b4b96f2ae690c77253b2cc0
Tasrif Khan <mdtasrifkhan453@gmail.com>
2025-10-20 07:21:16 +0600

    Added comments, validation, and formatted output

--- Abc.c
+++ Abc.c
@@ -4,6 +4,7 @@
 #define MAX_SUBJECTS 10
 #define MAX_NAME_LEN 20
 
+/* Function to calculate total marks of one student */
 int calculate_total(int marks[], int subjectCount) {
     int total = 0;
     for (int j = 0; j < subjectCount; j++) {
@@ -12,7 +13,9 @@ int calculate_total(int marks[], int subjectCount) {
     return total;
 }
 
+/* Function to find topper index */
 int find_topper(int totals[], int studentCount) {
+    if (studentCount <= 0) return -1;
     int maxIndex = 0;
     for (int i = 1; i < studentCount; i++) {
         if (totals[i] > totals[maxIndex]) {
@@ -27,28 +30,54 @@ int main() {
     int marks[MAX_STUDENTS][MAX_SUBJECTS], totals[MAX_STUDENTS];
     char studentNames[MAX_STUDENTS][MAX_NAME_LEN];
 
-    printf("Enter number of students: ");
+    printf("Enter number of students (max %d): ", MAX_STUDENTS);
     scanf("%d", &studentCount);
-    printf("Enter number of subjects: ");
-    scanf("%d", &subjectCount);
+    if (studentCount <= 0 || studentCount > MAX_STUDENTS) {
+        printf("Invalid number of students.\n");
+        return 1;
+    }
 
+    printf("Enter number of subjects (max %d): ", MAX_SUBJECTS);
+    scanf("%d", &subjectCount);
+    if (subjectCount <= 0 || subjectCount > MAX_SUBJECTS) {
+        printf("Invalid number of subjects.\n");
+        return 1;
+    }
+
+    /* Take input */
     for (int i = 0; i < studentCount; i++) {
-        printf("Enter name: ");
+        printf("\nEnter name of student %d: ", i + 1);
         scanf("%s", studentNames[i]);
+        printf("Enter marks for %d subjects:\n", subjectCount);
         for (int j = 0; j < subjectCount; j++) {
             scanf("%d", &marks[i][j]);
+            if (marks[i][j] < 0) {
+                printf("Invalid mark! Defaulting to 0.\n");
+                marks[i][j] = 0;
+            }
         }
     }
 
+    /* Calculate totals */
     for (int i = 0; i < studentCount; i++) {
         totals[i] = calculate_total(marks[i], subjectCount);
     }
 
+    /* Find topper */
     int topperIndex = find_topper(totals, studentCount);
 
+    printf("\n--- Student Results ---\n");
+    printf("%-15s | Total\n", "Name");
+    printf("-----------------------------\n");
     for (int i = 0; i < studentCount; i++) {
-        printf("%s %d\n", studentNames[i], totals[i]);
+        printf("%-15s | %d\n", studentNames[i], totals[i]);
     }
 
-    printf("Topper: %s\n", studentNames[topperIndex]);
+    if (topperIndex != -1) {
+        printf("\nTopper: %s with total %d\n", studentNames[topperIndex], totals[topperIndex]);
+    } else {
+        printf("\nNo topper found.\n");
+    }
+
+    return 0;
 }
