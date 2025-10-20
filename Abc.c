f276c781a0186de01dd9321e4ea4461dc5a67da6
Tasrif Khan <mdtasrifkhan453@gmail.com>
2025-10-20 07:11:37 +0600

    Refactored code structure and indentation

--- Abc.c
+++ Abc.c
@@ -1,9 +1,40 @@
-#include<stdio.h>
-int main(){int a[3][3]={{50,60,70},{80,90,100},{30,40,50}};char
-n[3][10]={"Ali","Bob","Cat"};int i,j,s;float avg;
-for(i=0;i<3;i++){s=0;for(j=0;j<3;j++){s+=a[i][j];}avg=s/3.0;if(avg>=80)p
-rintf("%s Grade A avg=%.2f\n",n[i],avg);else if(avg>=60)printf("%s Grade
-B avg=%.2f\n",n[i],avg);else if(avg>=40)printf("%s Grade C
-avg=%.2f\n",n[i],avg);else printf("%s Fail avg=%.2f\n",n[i],avg);}int
-max=0,pos=0;for(i=0;i<3;i++){s=0;for(j=0;j<3;j++){s+=a[i][j];}if(s>max){
-max=s;pos=i;}}printf("Topper: %s with total %d\n",n[pos],max);return 0;}
+#include <stdio.h>
+
+int main() {
+    int n, s, i, j;
+    int a[10][10], t[10], big = 0, k;
+    char name[10][20];
+
+    printf("Enter number of students: ");
+    scanf("%d", &n);
+    printf("Enter number of subjects: ");
+    scanf("%d", &s);
+
+    for (i = 0; i < n; i++) {
+        printf("Enter name: ");
+        scanf("%s", name[i]);
+        for (j = 0; j < s; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    for (i = 0; i < n; i++) {
+        t[i] = 0;
+        for (j = 0; j < s; j++) {
+            t[i] = t[i] + a[i][j];
+        }
+    }
+
+    for (i = 0; i < n; i++) {
+        if (t[i] > big) {
+            big = t[i];
+            k = i;
+        }
+    }
+
+    for (i = 0; i < n; i++) {
+        printf("%s %d\n", name[i], t[i]);
+    }
+
+    printf("Topper: %s\n", name[k]);
+}
