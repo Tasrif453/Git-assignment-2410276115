4461722ebae82a8248c6e2ca7e43fd637da5a1b0
Tasrif Khan <mdtasrifkhan453@gmail.com>
2025-10-20 07:19:11 +0600

    Added functions for average and grade calculation

--- Abc.c
+++ Abc.c
@@ -1,9 +1,31 @@
 #include <stdio.h>
 
+#define MAX_STUDENTS 10
+#define MAX_SUBJECTS 10
+#define MAX_NAME_LEN 20
+
+int calculate_total(int marks[], int subjectCount) {
+    int total = 0;
+    for (int j = 0; j < subjectCount; j++) {
+        total += marks[j];
+    }
+    return total;
+}
+
+int find_topper(int totals[], int studentCount) {
+    int maxIndex = 0;
+    for (int i = 1; i < studentCount; i++) {
+        if (totals[i] > totals[maxIndex]) {
+            maxIndex = i;
+        }
+    }
+    return maxIndex;
+}
+
 int main() {
     int studentCount, subjectCount;
-    int marks[10][10], total[10], highestTotal = 0, topperIndex;
-    char studentNames[10][20];
+    int marks[MAX_STUDENTS][MAX_SUBJECTS], totals[MAX_STUDENTS];
+    char studentNames[MAX_STUDENTS][MAX_NAME_LEN];
 
     printf("Enter number of students: ");
     scanf("%d", &studentCount);
@@ -19,21 +41,13 @@ int main() {
     }
 
     for (int i = 0; i < studentCount; i++) {
-        total[i] = 0;
-        for (int j = 0; j < subjectCount; j++) {
-            total[i] += marks[i][j];
-        }
+        totals[i] = calculate_total(marks[i], subjectCount);
     }
 
-    for (int i = 0; i < studentCount; i++) {
-        if (total[i] > highestTotal) {
-            highestTotal = total[i];
-            topperIndex = i;
-        }
-    }
+    int topperIndex = find_topper(totals, studentCount);
 
     for (int i = 0; i < studentCount; i++) {
-        printf("%s %d\n", studentNames[i], total[i]);
+        printf("%s %d\n", studentNames[i], totals[i]);
     }
 
     printf("Topper: %s\n", studentNames[topperIndex]);
